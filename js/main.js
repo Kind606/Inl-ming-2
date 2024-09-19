bb11719c6a159f5e53616584a7a828f4fc9b7d81
Kind606 <kevin.hellgren@hotmail.com>
2024-09-19 10:23:47 +0200

    Hb meny å annat

--- js/main.js
+++ js/main.js
@@ -24,11 +24,16 @@ function openMenMenu() {
     document.querySelector(".home-meny").classList.toggle("active-menu");
  }
 
+ function openHbMenu() {
+    document.getElementById("myDropdown").classList.toggle("show");
+    document.querySelector(".hb-meny").classList.toggle("active-menu");
+ }
+
  window.onclick = function(event) {
     // Om klicket inte är på någon av knapparna
-    if (!event.target.matches('.Menbtn') && !event.target.matches('.Femalebtn') && !event.target.matches('.Childbtn') && !event.target.matches('.Beautybtn') && !event.target.matches('.Homebtn')) {
+    if (!event.target.matches('.Menbtn') && !event.target.matches('.Femalebtn') && !event.target.matches('.Childbtn') && !event.target.matches('.Beautybtn') && !event.target.matches('.Homebtn') && !event.target.matches('.Hbbtn')) {
         // Hämta alla element med klasserna .navbar-2, .herr-meny och .Dam-meny
-        var dropdowns = document.querySelectorAll(".navbar-2, .herr-meny, .Dam-meny, .barn-meny, .beauty-meny, .home-meny");
+        var dropdowns = document.querySelectorAll(".navbar-2, .herr-meny, .Dam-meny, .barn-meny, .beauty-meny, .home-meny, .hb-meny");
         var i;
         for (i = 0; i < dropdowns.length; i++) {
             var openDropdown = dropdowns[i];
